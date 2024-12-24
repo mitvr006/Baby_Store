72aaa5b3a6894535d9b821d3982580e302a106cc
mitvr006 <pawankumar3439744@gmail.com>
2024-12-24 17:58:23 +0530

    image problem solve

--- mycutie/blog/urls.py
+++ mycutie/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 # from django.contrib.auth import views as auth_views
 from .views import signup, login_view, logout_view, add_product, product_list
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('signup/', signup, name='signup'),  
@@ -11,6 +13,9 @@ urlpatterns = [
 
 ]
 
+
+if settings.DEBUG:  # Serve media only during development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
