a034fd06345a09c1e00b3701ea32e2dc9c387079
mitvr006 <pawankumar3439744@gmail.com>
2024-12-24 16:00:28 +0530

    hhhh

--- mycutie/mycutie/views.py
+++ mycutie/mycutie/views.py
@@ -1,11 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from blog.models import Product
 
 def h(request):
     return HttpResponse("hello")
 
 def home(request):
-    return render(request, "shoped.html", {'user': request.user})
+    products = Product.objects.all()
+    return render(request, 'shoped.html', {'products': products})
 
 def akash(request):
     return render(request, "header.html")
